7c8d2d08d3969f1cbed2e54fbe290ba8156b0ebe
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-02 21:52:16 -0800

    Added loggin for daemon server

--- hydra/server/main.c
+++ hydra/server/main.c
@@ -14,13 +14,17 @@
 
 void	die(char *msg, FILE *fd)
 {
-	puts(msg);
-}
-
-void	die(char *msg)
-{
+	if (fd)
+		fputs(msg, fd);
 	puts(msg);
 	exit(1);
+}
+
+void	ft_log(char *msg, FILE *fd)
+{
+	if (fd)
+		fputs(msg, fd);
+	puts(msg);
 }
 
 void	parse_args(int ac, const char *av[], int *daemon, int *port)
@@ -31,14 +35,14 @@ void	parse_args(int ac, const char *av[], int *daemon, int *port)
 	if (ac != 2 && ac != 3)
 	{
 		asprintf(&msg, "usage %s: [port] [-D]\n", av[0]);
-		die(msg);
+		die(msg, NULL);
 	}
-	if ((*port = atoi(av[1])) < 0)
-		die("Port is incorrect\n");
+	if ((*port = atoi(av[1])) == 0)
+		die("Port is incorrect\n", NULL);
 	*daemon = (ac == 3) && (strcmp(av[2], "-D") == 0) ? 1 : 0;
 }
 
-int		start_server(int port, int out_fd)
+int		start_server(int port, FILE *out_fd)
 {
 	struct sockaddr_in	addr;
 	socklen_t			len;
@@ -49,18 +53,18 @@ int		start_server(int port, int out_fd)
 
 	c_sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (c_sock == -1)
-		die("Issue with socket()\n");
+		die("Issue with socket()\n", out_fd);
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(port);
 	if (bind(c_sock, (struct sockaddr *)&addr, sizeof(addr)) != 0)
-		die("bind() error\n");
+		die("bind() error\n", out_fd);
 	while (1)
 	{
 		if (listen(c_sock, 10) < 0)
-			die("listen() error\n");
+			die("listen() error\n", out_fd);
 		if ((n_sock = accept(c_sock, (struct sockaddr *)&addr, &len)) < 0)
-			die("accept() error\n");
+			die("accept() error\n", out_fd);
 		while ((nread = recv(n_sock, buf, BUFF_SIZE, 0)) > 0)
 		{
 			buf[nread] = '\0';
@@ -80,13 +84,15 @@ int		main(int ac, const char *av[])
 	int					daemon;
 	int					pid;
 	char				*msg;
+	FILE				*fd;
 	
+	fd = NULL;
 	parse_args(ac, av, &daemon, &port);
 	if (daemon)
 	{
 		pid = fork();
 		if (pid < 0)
-			die("fork() fail\n");
+			die("fork() fail\n", NULL);
 		else if (pid > 0)
 		{
 			printf("child's pid = %d\n", pid);
@@ -100,10 +106,9 @@ int		main(int ac, const char *av[])
 			close(STDIN_FILENO);
 			close(STDOUT_FILENO);
 			close(STDERR_FILENO);
-			// todo: set up log
-			start_server(port, 1);
+			fd = fopen(LOG_PATH, "w+");
 		}
 	}
-	start_server(port, 1);
+	start_server(port, NULL);
 	return (0);
 }
