c6e468b2b2758ea82ad32a7963c618f286fd2d23
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-09 10:53:52 -0800

    Hind initial commit

---
+++ hind/auth.rb
@@ -0,0 +1,10 @@
+require "oauth2"
+
+UID = "2b9a0883f878ec13e7556f83833d69d448e7868d5e53812b737d0989710d6149"
+SECRET = "d2fe665865cc3805ef59b8e54cd6ee88c255a658aca3733340d8a1c9d88923a8"
+
+# Create the client with your credentials
+client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
+# Get an access token
+token = client.client_credentials.get_token
+print "#{token}\n"
