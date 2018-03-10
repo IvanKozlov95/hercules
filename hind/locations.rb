429f656dc07874dc97cf9dbb4e7745b70ed367b5
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-03-09 17:34:39 -0800

    Implemented feature: if API is not responding, script should retry until API respond

--- hind/locations.rb
+++ hind/locations.rb
@@ -6,7 +6,7 @@
 #    By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/03/09 16:12:03 by ivankozlov        #+#    #+#              #
-#    Updated: 2018/03/09 16:50:40 by ivankozlov       ###   ########.fr        #
+#    Updated: 2018/03/09 17:32:48 by ivankozlov       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,37 +22,37 @@ users = {}
 ARGV.each do |a|
 	File.open(a, "r") do |f|
 		f.each_line do |line|
-			users[line.delete("\n")] = "offline"
+			users[line.delete("\n")] = "unknown"
 		end
 	end
 	rescue Errno::ENOENT, TypeError
 		puts "File #{a} not found\n"
 end
 
-users.each do |login, status|
-	response = token.get("/v2/users/#{login}")
-	if response.status == 200 && response.parsed["location"]
-		users[login] = response.parsed["location"]
+max_errors = 10
+error_count = 0
+while users.count > 0 do
+	users.each do |login, status|
+		response = token.get("/v2/users?filter[login]=#{login}")
+		if response.parsed == []
+			puts "#{login} - does not exist"
+			users.delete(login)
+		else
+			response = token.get("/v2/users/#{login}")
+			if response.status == 200 && response.parsed["location"]
+				puts "#{login} - #{response.parsed["location"]}"
+				users.delete(login)
+			else
+				puts "#{login} - offline"
+				users.delete(login)
+			end
+		end
+		rescue OAuth2::Error
+			error_count += 1
+			if (error_count >= max_errors)
+				puts "42 API is probably dead. I'm tired of trying.\n"
+				exit -1
+			end
+			break ;
 	end
-	rescue OAuth2::Error => e
-		users[login] = "Does not exist"
-end
-
-# resp = nil
-# count = 0
-# page = 0
-# begin
-# 	page += 1
-# 	url = "v2/campus/7/locations?filter=active&page=#{page}" 
-# 	resp = token.get(url)
-# 	resp.parsed.each do |entry|
-# 		if (users[entry["user"]["login"]] == "offline")
-# 			count += 1
-# 			users[entry["user"]["login"]] = entry["host"]
-# 		end
-# 	end
-# end while resp.headers["link"] && resp.headers["link"]["next"] && count < users.count
-
-users.each do |login, status|
-	puts "#{login} - #{status}"
 end
