bc08f2dd6da4916dbc7c2eea851091fb7d390f6f
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-07 22:49:32 -0700

    Grooming

--- bull/forest.s
+++ bull/forest.s
@@ -1,10 +1,11 @@
 .name "forest"
 .comment "1forest1"
 
-l2:		sti r1, %:live, %1
-		sti r1, %:live2, %1
-		and r1, %0, r2
-		fork %:live2
+l2:
+	sti r1, %:live, %1
+	sti r1, %:live2, %1
+	and r1, %0, r2
+	fork %:live2
 
 live:
 	live %1
